46183b0826c936595a934203f40f08eb6568818d
sodiolabdullaholid <abdullahsodiol@gmail.com>
2025-05-10 16:58:37 +0600

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int myNumber = (int)(Math.random()*100);
+        int userNumber = 0;
+        do{
+            System.out.println("Guess The secret number :");
+            userNumber = sc.nextInt();
+            if ( userNumber == myNumber ){
+                System.out.println("wohho you are the besttttt !!!");
+                break;
+            }
+            else if ( userNumber > myNumber ){
+                System.out.println("ops the number is small ");
+                break;
+            }
+            else{
+                System.out.println("opps the number is too large ");
+                break;
+            }
 
+        } while (userNumber >= 0 ) ;
+
+        System.out.print("My number was : ");
+        System.out.println(myNumber);
     }
 }
